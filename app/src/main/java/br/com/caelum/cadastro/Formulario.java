82daa6a4b4019f7187900c94b79867e5271d4841
Rodrigo Carvalho <rodrigo.carvalho@fcamara.com.br>
2016-05-29 21:24:07 -0300

    #Criando funcionalidade para capturar imagens da camera

--- app/src/main/java/br/com/caelum/cadastro/Formulario.java
+++ app/src/main/java/br/com/caelum/cadastro/Formulario.java
@@ -2,13 +2,19 @@ package br.com.caelum.cadastro;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
+import java.io.File;
 import java.io.Serializable;
+import java.net.URI;
 
 import br.com.caelum.cadastro.dao.AlunoDao;
 import br.com.caelum.cadastro.modelo.Aluno;
@@ -16,9 +22,10 @@ import br.com.caelum.cadastro.modelo.Aluno;
 /**
  * Created by rodrigo on 21/03/16.
  */
-public class Formulario extends Activity{
+public class Formulario extends Activity {
 
     private FormularioHelper formularioHelper;
+    private String caminhoArquivo;
 
 
     @Override
@@ -54,14 +61,47 @@ public class Formulario extends Activity{
                     alunoDao.altera(aluno);
                 }
 
-
-                alunoDao.salva(aluno);
                 alunoDao.close();
 
                 finish();
             }
         });
 
+        ImageView foto = formularioHelper.getFoto();
+
+        foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irParaCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                caminhoArquivo = Environment.getExternalStorageDirectory().toString()
+                          + "/" + System.currentTimeMillis() + ".png";
+
+                File arquivo = new File(caminhoArquivo);
+
+                Uri localImagem = Uri.fromFile(arquivo);
+                irParaCamera.putExtra(MediaStore.EXTRA_OUTPUT, localImagem);
+
+                //Aguardando Resultado
+                startActivityForResult(irParaCamera, 123);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 123) {
+
+            if (resultCode == Activity.RESULT_OK) {
+                //foto OK
+                formularioHelper.carregarImagem(caminhoArquivo);
+            } else {
+                //foto cancelada
+                caminhoArquivo = null;
+            }
+        }
 
     }
 }
--- app/src/main/java/br/com/caelum/cadastro/FormularioHelper.java
+++ app/src/main/java/br/com/caelum/cadastro/FormularioHelper.java
@@ -1,6 +1,9 @@
 package br.com.caelum.cadastro;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import br.com.caelum.cadastro.modelo.Aluno;
@@ -15,20 +18,22 @@ public class FormularioHelper {
     private EditText editTelefone;
     private EditText editEndereco;
     private RatingBar rantingNota;
+    private ImageView foto;
+    private Aluno aluno;
 
     public FormularioHelper(Formulario formulario) {
-
 
         editNome = (EditText) formulario.findViewById(R.id.nome);
         editSite = (EditText) formulario.findViewById(R.id.site);
         editTelefone = (EditText) formulario.findViewById(R.id.telefone);
         editEndereco = (EditText) formulario.findViewById(R.id.endereco);
         rantingNota = (RatingBar) formulario.findViewById(R.id.nota);
+        foto = (ImageView) formulario.findViewById(R.id.foto);
+        aluno = new Aluno();
     }
 
     public Aluno pegaAlunoDoFormulario() {
 
-        Aluno aluno = new Aluno();
         aluno.setNome(editTextToString(editNome));
         aluno.setSite(editTextToString(editSite));
         aluno.setEndereco(editTextToString(editEndereco));
@@ -38,16 +43,37 @@ public class FormularioHelper {
         return aluno;
     }
 
+    public void colocaAlunoFormulario(Aluno alunoParaSerAlterado) {
+
+        aluno = alunoParaSerAlterado;
+        editNome.setText(alunoParaSerAlterado.getNome());
+        editSite.setText(alunoParaSerAlterado.getSite());
+        editTelefone.setText(alunoParaSerAlterado.getTelefone());
+        editEndereco.setText(alunoParaSerAlterado.getEndereco());
+        rantingNota.setRating(alunoParaSerAlterado.getNota().floatValue());
+
+        if (alunoParaSerAlterado.getFoto() != null ) {
+            carregarImagem(alunoParaSerAlterado.getFoto());
+        }
+    }
+
     private String editTextToString(EditText editText) {
         return editText.getText().toString();
     }
 
-    public void colocaAlunoFormulario(Aluno aluno) {
+    public ImageView getFoto() {
+        return foto;
+    }
 
-        editNome.setText(aluno.getNome());
-        editSite.setText(aluno.getSite());
-        editTelefone.setText(aluno.getTelefone());
-        editEndereco.setText(aluno.getEndereco());
-        rantingNota.setRating(aluno.getNota().floatValue());
+    public void carregarImagem(String caminhoArquivo) {
+
+        aluno.setFoto(caminhoArquivo);
+        Bitmap imagem = BitmapFactory.decodeFile(caminhoArquivo);
+
+        if (imagem == null) {
+            //Criando imagem reduzida
+            Bitmap imagemReduzida = Bitmap.createScaledBitmap(imagem, 100, 100, true);
+            foto.setImageBitmap(imagemReduzida);
+        }
     }
 }
